55db4f96abc24d64bc1aa03780027e5da30d70e7
deein <deein@git.com>
2015-01-21 00:03:23 +0100

    Push 3

--- app/views/productions.php
+++ app/views/productions.php
@@ -1,41 +1,45 @@
-<!-- DOC -->
-<div class="col-sm-12 project" data-background="images/portfolio/allstargames.jpg">
-	<a href="/all-star-games">
-		<div class="overlay">
-			<div class="title">
-				<h4>JK TV</h4>
-				<h2>Allstar Games</h2>
-			</div>
+<section class="container-fluid">
+	<section class="row">
+		<!-- DOC -->
+		<div class="col-sm-12 project" data-background="images/portfolio/allstargames.jpg">
+			<a href="/all-star-games">
+				<div class="overlay">
+					<div class="title">
+						<h4>JK TV</h4>
+						<h2>Allstar Games</h2>
+					</div>
+				</div>
+			</a>
 		</div>
-	</a>
-</div>
-<div class="col-sm-12 project" data-background="images/portfolio/aaclabel.jpg">
-	<a href="#">
-		<div class="overlay">
-			<div class="title">
-				<h4>ubisoft</h4>
-				<h2>duel of champions season 3</h2>
-			</div>
+		<div class="col-sm-12 project" data-background="images/portfolio/aaclabel.jpg">
+			<a href="#">
+				<div class="overlay">
+					<div class="title">
+						<h4>ubisoft</h4>
+						<h2>duel of champions season 3</h2>
+					</div>
+				</div>
+			</a>
 		</div>
-	</a>
-</div>
-<div class="col-sm-12 project" data-background="images/portfolio/duelofchampions.jpg">
-	<a href="#">
-		<div class="overlay">
-			<div class="title">
-				<h4>JK TV</h4>
-				<h2>Allstar Games</h2>
-			</div>
+		<div class="col-sm-12 project" data-background="images/portfolio/duelofchampions.jpg">
+			<a href="#">
+				<div class="overlay">
+					<div class="title">
+						<h4>JK TV</h4>
+						<h2>Allstar Games</h2>
+					</div>
+				</div>
+			</a>
 		</div>
-	</a>
-</div>
-<div class="col-sm-12 project" data-background="images/portfolio/eswc.jpg">
-	<a href="#">
-		<div class="overlay">
-			<div class="title">
-				<h4>JK TV</h4>
-				<h2>Allstar Games</h2>
-			</div>
+		<div class="col-sm-12 project" data-background="images/portfolio/eswc.jpg">
+			<a href="#">
+				<div class="overlay">
+					<div class="title">
+						<h4>JK TV</h4>
+						<h2>Allstar Games</h2>
+					</div>
+				</div>
+			</a>
 		</div>
-	</a>
-</div>
+	</section>
+</section>
